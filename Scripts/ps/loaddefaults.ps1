91b2ea8c5470a74d0adc20512520d09ceab64720
Fablicator <accts.fablicator@gmail.com>
2020-01-13 21:40:30 -0500

    Force load defaults and silent continue

--- Scripts/ps/loaddefaults.ps1
+++ Scripts/ps/loaddefaults.ps1
@@ -1,8 +1,6 @@
-try {
-    Remove-Item -Path "./_materials" -Recurse > $null 
-    Remove-Item -Path "./_printers" -Recurse > $null 
-    Remove-Item -Path "./_styles" -Recurse > $null 
-    Remove-Item -Path "./_supports" -Recurse > $null 
-}catch{}
+Remove-Item -Path "./_materials" -Recurse -erroraction 'silentlycontinue' -Force
+Remove-Item -Path "./_printers" -Recurse -erroraction 'silentlycontinue' -Force
+Remove-Item -Path "./_styles" -Recurse -erroraction 'silentlycontinue' -Force
+Remove-Item -Path "./_supports" -Recurse -erroraction 'silentlycontinue' -Force
 
 Copy-Item -Path "./defaults/*" -Destination "./" -Recurse
