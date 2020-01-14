8267e9c945c57560f9d2e9e10dff2b806dcd47f4
Fablicator <accts.fablicator@gmail.com>
2020-01-13 21:28:57 -0500

    Remove red text  from LoadDefaults script

--- Scripts/ps/loaddefaults.ps1
+++ Scripts/ps/loaddefaults.ps1
@@ -1,8 +1,6 @@
-try {
-    Remove-Item -Path "./_materials" -Recurse > $null 
-    Remove-Item -Path "./_printers" -Recurse > $null 
-    Remove-Item -Path "./_styles" -Recurse > $null 
-    Remove-Item -Path "./_supports" -Recurse > $null 
-}catch{}
+Remove-Item -Path "./_materials" -Recurse -erroraction 'silentlycontinue' 
+Remove-Item -Path "./_printers" -Recurse -erroraction 'silentlycontinue' 
+Remove-Item -Path "./_styles" -Recurse -erroraction 'silentlycontinue' 
+Remove-Item -Path "./_supports" -Recurse -erroraction 'silentlycontinue'
 
 Copy-Item -Path "./defaults/*" -Destination "./" -Recurse
