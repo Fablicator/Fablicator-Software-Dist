353cc3e24017e175746389a8054954a13c12fcb0
Fablicator <accts.fablicator@gmail.com>
2020-01-22 16:42:27 -0500

    Finish MX setup script

--- ps/setupmx.ps1
+++ ps/setupmx.ps1
@@ -1,3 +1,6 @@
+# Clone common software
+& "$PSScriptRoot/clonecommon.ps1"
+
 $dist_root = "$PSScriptRoot/../.."
 $dist_repo = "https://github.com/Fablicator/Fablicator-Software-Dist.git"
 
@@ -6,4 +9,7 @@ $dist_repo = "https://github.com/Fablicator/Fablicator-Software-Dist.git"
 git clone --branch "kisslicer-mx" $dist_repo "$dist_root/KISSlicer"
 
 # Setup KISSlicer defaults
-"$dist_root/KISSlicer/Scripts/ps/loaddefaults.ps1"
+& "$dist_root/KISSlicer/Scripts/ps/loaddefaults.ps1"
+
+# Setup MX Marlin Calibration
+Copy-Item -Path "$dist_root/Marlin/CalibrationDefaults/MX/Calibration.h" -Destination "$dist_root/Marlin/Marlin/Calibration.h"
