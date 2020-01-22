189f5a4c195acb49854f00b36cf2c79da73cb835
Fablicator <accts.fablicator@gmail.com>
2020-01-22 16:31:17 -0500

    Made script paths explicit
    # Conflicts:
    #	Scripts/ps/loaddefaults.ps1
    
    # Conflicts:
    #	Scripts/ps/loaddefaults.ps1

--- Scripts/ps/loaddefaults.ps1
+++ Scripts/ps/loaddefaults.ps1
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 Remove-Item -Path "./_materials" -Recurse -erroraction 'silentlycontinue' -Force
 Remove-Item -Path "./_printers" -Recurse -erroraction 'silentlycontinue' -Force
 Remove-Item -Path "./_styles" -Recurse -erroraction 'silentlycontinue' -Force
 Remove-Item -Path "./_supports" -Recurse -erroraction 'silentlycontinue' -Force
+=======
+$kiss_root = "$PSScriptRoot/../.."
+$defaults_folder = "$kiss_root/defaults"
+>>>>>>> a99d4ab... Made script paths explicit
 
-Copy-Item -Path "./defaults/*" -Destination "./" -Recurse
+Remove-Item -Path "$kiss_root/_materials" -Recurse -erroraction 'silentlycontinue' -Force
+Remove-Item -Path "$kiss_root/_printers" -Recurse -erroraction 'silentlycontinue' -Force
+Remove-Item -Path "$kiss_root/_styles" -Recurse -erroraction 'silentlycontinue' -Force
+Remove-Item -Path "$kiss_root/_supports" -Recurse -erroraction 'silentlycontinue' -Force
+
+Copy-Item -Path "$defaults_folder/*" -Destination "$kiss_root/" -Recurse
