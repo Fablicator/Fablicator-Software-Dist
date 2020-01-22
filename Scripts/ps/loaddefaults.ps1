63253bcdd9efe08b579203ecc1678520e1272fce
Fablicator <accts.fablicator@gmail.com>
2020-01-22 17:09:07 -0500

    Fix problem with load defaults script

--- Scripts/ps/loaddefaults.ps1
+++ Scripts/ps/loaddefaults.ps1
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-Remove-Item -Path "./_materials" -Recurse -erroraction 'silentlycontinue' -Force
-Remove-Item -Path "./_printers" -Recurse -erroraction 'silentlycontinue' -Force
-Remove-Item -Path "./_styles" -Recurse -erroraction 'silentlycontinue' -Force
-Remove-Item -Path "./_supports" -Recurse -erroraction 'silentlycontinue' -Force
-=======
 $kiss_root = "$PSScriptRoot/../.."
 $defaults_folder = "$kiss_root/defaults"
->>>>>>> a99d4ab... Made script paths explicit
-
 Remove-Item -Path "$kiss_root/_materials" -Recurse -erroraction 'silentlycontinue' -Force
 Remove-Item -Path "$kiss_root/_printers" -Recurse -erroraction 'silentlycontinue' -Force
 Remove-Item -Path "$kiss_root/_styles" -Recurse -erroraction 'silentlycontinue' -Force
